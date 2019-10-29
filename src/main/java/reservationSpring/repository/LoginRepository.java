b22d20dc4238d45b6187d9f52f9e665287d03c38
fmohamed99 <mohamed.fatima69@yahoo.fr>
2019-10-29 14:47:06 +0100

    update

--- src/main/java/reservationSpring/repository/LoginRepository.java
+++ src/main/java/reservationSpring/repository/LoginRepository.java
@@ -1,15 +1,22 @@
 package reservationSpring.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import reservationSpring.model.Login;
 
 
 public interface LoginRepository extends JpaRepository<Login, Long>{
-	public Optional<Login> findById(Long id);
+	
+	@Query("select distinct l from Login where id=:id")
+	public Optional<Login> findByIdWithClient(@Param("id") Long id);
 	
 	public Optional<Login> findByLogin(String login); 
+	
+	public List<Login> findAllCustomWithClient();
 
 }
