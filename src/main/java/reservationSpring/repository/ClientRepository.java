2abdee8406ee5e82ed373e58b9abe826ba66869e
fmohamed99 <mohamed.fatima69@yahoo.fr>
2019-10-29 13:09:34 +0100

    update

--- src/main/java/reservationSpring/repository/ClientRepository.java
+++ src/main/java/reservationSpring/repository/ClientRepository.java
@@ -18,18 +18,18 @@ public interface ClientRepository extends JpaRepository<Client, Long>{
 	@Query("select distinct c from Client c left join fetch c.reservations where c.id = :id")
 	public Optional <Client> findByIdWithReservations(@Param("id")Long id);
 	
-	@Query("select distinct c from Client c left join fetch c.reservations")
-	public List<Client> findAllCustomWithReservations();
-
-	@Query("select distinct cei from ClientEI")
-	public List<ClientEI> findAllCustomClientEI();
-	
-	@Query("select distinct cm from ClientMoral")
-	public List<ClientMoral> findAllCustomClientMoral();
-	
-	@Query("select distinct cp from ClientPhysique")
-	public List<ClientPhysique> findAllCustomClientPhysique();
-	
-	@Query("select distinct c from Client c left join fetch c.login =:login")
-	public Optional<Client> findByLoginWithReservations(@Param("login") Login login);
+//	@Query("select distinct c from Client c left join fetch c.reservations")
+//	public List<Client> findAllCustomWithReservations();
+//
+//	@Query("select distinct cei from ClientEI")
+//	public List<ClientEI> findAllCustomClientEI();
+//	
+//	@Query("select distinct cm from ClientMoral")
+//	public List<ClientMoral> findAllCustomClientMoral();
+//	
+//	@Query("select distinct cp from ClientPhysique")
+//	public List<ClientPhysique> findAllCustomClientPhysique();
+//	
+//	@Query("select distinct c from Client c left join fetch c.login =:login")
+//	public Optional<Client> findByLoginWithReservations(@Param("login") Login login);
 }
