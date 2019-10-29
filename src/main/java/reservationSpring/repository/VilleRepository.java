42d2ef190ac1075772ea170013c53e13fd1e2968
unknown <gratas.simon@gmail.com>
2019-10-29 15:41:53 +0100

    new file:   src/main/java/reservationSpring/service/AeroportService.java new file:   src/test/java/reversationSpring/TestVille.java

--- src/main/java/reservationSpring/repository/VilleRepository.java
+++ src/main/java/reservationSpring/repository/VilleRepository.java
@@ -1,10 +1,21 @@
 package reservationSpring.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import reservationSpring.model.Ville;
 
 public interface VilleRepository extends JpaRepository<Ville, Long>{
+	
+	@Query("select distinct v from Ville v left join fetch v.liaisons l  left join fetch l.key.aeroport where v.idVille=:key")
+	public Optional<Ville> findByKeyWithAeroport(@Param("key") Long key);
+
+	@Query("select distinct v from Ville v left join fetch v.liaisons l  left join fetch l.key.aeroport")
+	public List<Ville> findAllCustomWithAeroport();
 	
 	
 
