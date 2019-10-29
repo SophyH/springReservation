a9f2f3dac9eba570b4a0197cba808a6b7034ea18
SophyH <so.huk@laposte.net>
2019-10-29 15:37:27 +0100

    service

---
+++ src/main/java/reservationSpring/exception/PassagerCreationException.java
@@ -0,0 +1,9 @@
+package reservationSpring.exception;
+
+public class PassagerCreationException extends Exception {
+
+	public PassagerCreationException() {
+		super("création passager impossible");
+	}
+
+}
